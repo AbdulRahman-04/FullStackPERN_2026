cee6776f315123a34bf44e0ad24879301af7d4ed
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-08-23 08:29:41 +0530

    All About JS Loops

---
+++ 02JavaScript/01-Basics/4.js
@@ -0,0 +1,67 @@
+// Loops in JavaScript:
+// Loops are used to execute the same block of code multiple times
+// until a given condition becomes false.
+
+
+// for loop : it is an entry controlled loop.
+// in this loop the variable declaration, condition check and the increment or decrement all happens at same.
+
+// syntax:
+// for(i=0;i<=10;i++){
+//     if(i%2==0){
+//         console.log(i);
+//         if(i===8){
+//             break
+//         }
+        
+//     }
+// }
+
+
+// for(i=1;i<5;i++){
+//     if(i===3) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+
+
+// for(i=5; i>=1; i--){
+//     console.log(i);
+    
+// }
+
+// while loop: it is also one of the netry controlled loop.
+// in this loop the variable declaration is done first, then condition is checked using while loop and the increment or decrement  happens at last.
+// syntax:
+// let num = 1
+// while(num<=20){
+//     if(num%2==0){
+//         console.log(num);
+//     }
+//     num++
+// }
+
+// let myNum = 5;
+// while(myNum>=1){
+//     console.log(myNum);
+//     myNum--
+// }
+
+
+// do while : it is an exit controlled loop.
+// in this loop the variable declaration, the code is executed, then increment or decrement happens nd conditions is checked at last.
+// syntax:
+// let number = 10
+// do {
+//     console.log(number);
+//     number++
+// } while(number<=15)
+
+// let count = 5
+// do {
+//     console.log(count);
+//     count--
+// }   while(count>=1) 
